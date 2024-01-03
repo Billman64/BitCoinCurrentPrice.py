ad90ec9e32158ce0c4cc16440ef40c20d19f3458
Bill Lugo <11304397+Billman64@users.noreply.github.com>
2024-01-02 23:41:09 -0500

    Gets updated date-time and current price in USD

--- BitCoinCurrentPrice.py
+++ BitCoinCurrentPrice.py
@@ -5,7 +5,9 @@ url = "https://api.coindesk.com/v1/bpi/currentprice.json"
 
 print("Getting the current price of BitCoin...")
 response = requests.get(url)
+
 if(response.status_code == 200):
-    print(response.json())
+    print(response.json()['time']['updated'])
+    print("$" + response.json()['bpi']['USD']['rate'])
 else:
     print("Could not get data. Status code: " + str(response.status_code))
