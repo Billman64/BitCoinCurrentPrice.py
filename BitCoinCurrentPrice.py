d7acf062f090cf2e2dcb0f3b7fd4f086a3fcc23b
Bill Lugo <11304397+Billman64@users.noreply.github.com>
2024-01-02 23:16:31 -0500

    Initial commit

---
+++ BitCoinCurrentPrice.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+
+print("Getting the current price of BitCoin...")
+response = requests.get(url)
+if(response.status_code == 200):
+    print(response.json())
+else:
+    print("Could not get data. Status code: " + str(response.status_code))
