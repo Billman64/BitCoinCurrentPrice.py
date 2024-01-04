50cdb0269c120682ebcb447719fc9e4713d32458
Bill Lugo <11304397+Billman64@users.noreply.github.com>
2024-01-03 23:17:59 -0500

    Added support for quiet mode (parameter "q")

--- BitCoinCurrentPrice.py
+++ BitCoinCurrentPrice.py
@@ -1,13 +1,28 @@
 import requests
 import json
+import sys
 
+# Verbosity/quiet check
+verbosity = True
+if(sys.argv[1:] and sys.argv[1].lower() == "q"):
+    verbosity = False
+    
 url = "https://api.coindesk.com/v1/bpi/currentprice.json"
 
-print("Getting the current price of BitCoin...")
+def p(str):
+    if(verbosity): print(str)
+
+
+p("Getting the current price of BitCoin...")
 response = requests.get(url)
 
 if(response.status_code == 200):
-    print(response.json()['time']['updated'])
+    p(response.json()['time']['updated'])
     print("$" + response.json()['bpi']['USD']['rate'])
+
+
+    
 else:
-    print("Could not get data. Status code: " + str(response.status_code))
+    p("Could not get data. Status code: " + str(response.status_code))
+
+
