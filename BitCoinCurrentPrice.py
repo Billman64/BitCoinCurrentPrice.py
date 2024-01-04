bf04e183b73ec2e66f46b064bf58bb08c24a4211
Bill Lugo <11304397+Billman64@users.noreply.github.com>
2024-01-04 00:55:42 -0500

    Implemented help option
    Shows argument usages to user.

--- BitCoinCurrentPrice.py
+++ BitCoinCurrentPrice.py
@@ -7,6 +7,21 @@ import sys
 verbosity = True
 url = "https://api.coindesk.com/v1/bpi/currentprice.json"
 
+def help():
+    import os
+    print("\n Name: " + os.path.basename(__file__))
+    print("\n Description: Gets the current value of BitCoin.")
+    print("\n Usage:\n")
+    print("\t [q] - Quiet mode. No verbosity, so that only the value is output.")
+    print("\t [eur, gbp, usd] - Specify currency.")
+    print("\t [v] - Verbose mode. (Default)")
+    print("")
+    print("Note: Compatible with Python 3.1+. Data source is from Coindesk.")
+    print("")
+    exit()
+
+
+
 def p(str): # simple function that prints only if verbosity allows
     if(verbosity): print(str)
 
@@ -19,19 +34,27 @@ for i in sys.argv:
         case "gbp":
             sym = "£"
             curr = "GBP"
+        case "usd":
+            sym = "$"
+            curr = "USD"
         case "q":
             verbosity = False
+        case "v":
+            verbosity = True
+        case "h" | "help":
+            help()
         case _:
             sym = "$"
             curr = "USD"
 
+# Get the data from the web API
 p("Getting the current price of BitCoin...")
 response = requests.get(url)
 
+# Output - parsing data
 if(response.status_code == 200):
     p(response.json()['time']['updated'])
     print(sym + response.json()['bpi'][curr]['rate'])
-
 else:
     p("Could not get data. Status code: " + str(response.status_code))
 
