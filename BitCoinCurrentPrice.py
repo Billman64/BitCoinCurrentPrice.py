b27256766b542cd1c44fd99c6ea50f6ceb8a091b
Bill Lugo <11304397+Billman64@users.noreply.github.com>
2024-01-04 00:11:55 -0500

    Implemented support for other fiats (EUR, GBP)
    To use: type in "eur", "gbp", or "usd" in a command-line argument.
    Also: quiet mode refactored into new argument match scanning. (Note: not compatible with Python versions earlier than 3.1).

--- BitCoinCurrentPrice.py
+++ BitCoinCurrentPrice.py
@@ -2,26 +2,36 @@ import requests
 import json
 import sys
 
-# Verbosity/quiet check
+
+# defaults
 verbosity = True
-if(sys.argv[1:] and sys.argv[1].lower() == "q"):
-    verbosity = False
-    
 url = "https://api.coindesk.com/v1/bpi/currentprice.json"
 
-def p(str):
+def p(str): # simple function that prints only if verbosity allows
     if(verbosity): print(str)
 
+# Get currency, verbosity level from command-line arguments
+for i in sys.argv:
+    match i:
+        case "eur":
+            sym = "€"
+            curr = "EUR"
+        case "gbp":
+            sym = "£"
+            curr = "GBP"
+        case "q":
+            verbosity = False
+        case _:
+            sym = "$"
+            curr = "USD"
 
 p("Getting the current price of BitCoin...")
 response = requests.get(url)
 
 if(response.status_code == 200):
     p(response.json()['time']['updated'])
-    print("$" + response.json()['bpi']['USD']['rate'])
+    print(sym + response.json()['bpi'][curr]['rate'])
 
-
-    
 else:
     p("Could not get data. Status code: " + str(response.status_code))
 
