99255003b78bea9f5b5dfdbb799d4127210c9ee4
Bill Lugo <11304397+Billman64@users.noreply.github.com>
2024-01-06 01:17:03 -0500

    Scalable support for converting to more currencies
    47 fiat currencies added (for now).

--- BitCoinCurrentPrice.py
+++ BitCoinCurrentPrice.py
@@ -5,7 +5,12 @@ import sys
 
 # defaults
 verbosity = True
-url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+url_base = "https://api.coindesk.com/v1/bpi/"
+url = url_base + "currentprice.json"
+url_alt = "currentprice/"
+#url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+curr_list = ["amd","ang","ars","awg","aud","bam","bbd","bdt","bgn","bwp","bzd","chf", "clp",
+             "cny", "cad", "cop", "crc", "cuc", "cup","cve","czk", "hkd", "hnl", "hrk", "htg", "huf", "inr","iqd","isk", "jpy", "krw", "kyd", "mxn", "nok", "nzd", "pab", "pen", "qar", "ron", "rub", "sek", "uah", "xas", "xau", "xbt", "xcd", "xdr"]
 
 def help():
     import os
@@ -16,6 +21,7 @@ def help():
     print("\t [eur, gbp, usd] - Specify currency.")
     print("\t [v] - Verbose mode. (Default)")
     print("")
+    print(" # of currencies supported: " + str(len(curr_list)+3))
     print("Note: Compatible with Python 3.1+. Data source is from Coindesk.")
     print("")
     sys.exit()
@@ -46,6 +52,11 @@ for i in sys.argv:
         case _:
             sym = "$"
             curr = "USD"
+    if (i in curr_list):
+        sym=""
+        curr = i.upper()
+        url = url_base + url_alt + i.upper() + ".json"
+
 
 # Get the data from the web API
 p("Getting the current price of BitCoin...")
@@ -54,7 +65,8 @@ response = requests.get(url)
 # Output - parsing data
 if(response.status_code == 200):
     p(response.json()['time']['updated'])
-    print(sym + response.json()['bpi'][curr]['rate'])
+    print(sym + response.json()['bpi'][curr]['rate'] + " " + response.json()['bpi'][curr]['description'])
+    p("\n" + response.json()['disclaimer'])
 else:
     p("Could not get data. Status code: " + str(response.status_code))
 
